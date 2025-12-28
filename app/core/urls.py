110f04f0507966a798aec4db77e697006ba1bcf9
lukasvaz <lukas.vasquez.verdejo@gmail.com>
2025-12-28 20:37:02 -0300

    fix  media

--- app/core/urls.py
+++ app/core/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.urls import re_path
+from django.views.static import serve
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -53,9 +56,15 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
+if settings.DEBUG:
+    urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT)
-
-print("-------------Media URL:", settings.MEDIA_URL)
-print("------------Media Root:", settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        ),
+    ]
